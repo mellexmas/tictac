c7d251d4f2174f09bc796a5eb6b466681206081e
mellexmas <empauhe@gmail.com>
2021-01-28 12:14:12 +0100

    avant de faire nimp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ParameterComponent } from './parameter/parameter.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { CellComponent } from './cell/cell.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ParameterComponent,
-    HomePageComponent
+    HomePageComponent,
+    CellComponent
   ],
   imports: [
     BrowserModule,
