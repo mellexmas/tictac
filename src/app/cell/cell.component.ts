bc570e3c4c31b654b8c783d0b9498eca69ed8b27
mellexmas <empauhe@gmail.com>
2021-01-28 12:37:20 +0100

    finalement ça s'est bien passé

--- src/app/cell/cell.component.ts
+++ src/app/cell/cell.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Cell } from '../cell';
 
 @Component({
   selector: 'app-cell',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CellComponent implements OnInit {
 
+  @Input()
+  public cell:  Cell|null = null;
   constructor() { }
 
   ngOnInit(): void {
